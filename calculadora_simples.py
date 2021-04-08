373e517d5c1facbff8c1ac022376f80b842375ac
willianyamauti <81825556+willianyamauti@users.noreply.github.com>
2021-04-08 17:33:05 -0300

    Update calculadora_simples.py

--- calculadora_simples.py
+++ calculadora_simples.py
@@ -1,19 +1,16 @@
 """
-
-operacoes +,-,/,*,**,
-
+operations suported +,-,/,*,**,
 """
 
 while True:
 
     print()
-    num_1 = input('Digite o primeiro numero ')
-    num_2 = input('Digite o segundo numero ')
-    operacao = input('Digite + para adição, - para subtração, * para multiplicaçao'
-                     '^ para potenciacão, / para divisão e qualquer outro caractere para sair ')
+    num_1 = input('Type the first number ')
+    num_2 = input('Type the second number ')
+    operacao = input('Type the desired operation(+,-,*,/,^ or any other key to exit:  ')
 
     if not num_1.isnumeric() or not num_2.isnumeric():
-        print ('Você precisa digitar um numero')
+        print ('You need to typer numbers!')
         continue
 
     num_1 = int(num_1)
@@ -30,5 +27,5 @@ while True:
     elif operacao == '^':
         print(num_1**num_2)
     else:
-        print ('caculadora encerrada')
+        print ('EXITING CALCULATOR')
         break
